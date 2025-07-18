ec2eb95bd3546c779721057d0628fdf993898090
otavioaugustoafm <otavioaugustoafm@gmail.com>
2025-07-18 16:03:10 -0300

    Update inputProcessing.py

--- FinanceBot/TestBot_v03/inputProcessing.py
+++ FinanceBot/TestBot_v03/inputProcessing.py
@@ -1,32 +1,44 @@
 from datetime import datetime
 
 def DBdateFormater(date): # formart the date to the DB model
-    date = date.split("/")
-    date = date[2] + "-" + date[1] + "-" + date[0]
-    return date
+    try:
+        print("Transformando data para o modelo do Banco de Dados.")
+        date = date.split("/")
+        date = date[2] + "-" + date[1] + "-" + date[0]
+        print("Data transformada com sucesso.")
+        return date
+    except:
+        print("Algum erro ocorreu durante a transformação da data.")
+        return False
 
 def checkType(type): # checks if the type is valid
+    print("Checando se o tipo é válido.")
     TYPES = {"TRANSPORTE", "LAZER", "COMPRAS", "ALIMENTAÇÃO", "OUTROS"}
     if type.upper() in {"ALIMENTACAO", "ALIMENTAÇAO", "ALIMENTACÃO", "ALIMENTAÇÃO"}:
         type = "ALIMENTAÇÃO"
     if type.upper() in TYPES:
+        print("O tipo é válido.")
         return type.capitalize()
     else:
+        print("O tipo não é válido.")
         return False
 
 def checkInput (input): # check how the user made his input
+    print("Checando a mensagem do usuário como um todo.")
     input = input.split(" ", 2)
-    type = checkType(input[1])
+    if len(input) == 1: # checks if the user entered less than two fields
+        print("O usuário digitou apenas um campo.")
+        return "Entrada inválida! Informe VALOR e TIPO no mínimo.\nExemplo: 29,99 Compras"
+    type = checkType(input[1]) # checks if the type is valid
     if type is False:
         return f"O tipo \"{input[1]}\" não é válido."
-    if len(input) == 1: # checks if the user entered less than two fields
-        return "Entrada inválida! Informe, no MÍNIMO, VALOR e TIPO.\n\nExemplo: 29,99 Compras"
-    elif len(input) == 2: # checks whether the user has entered the minimun number of fields
+    if len(input) == 2: # checks whether the user has entered the minimun number of fields
+        print("O usuário digitou dois campos.")
         answer = { 
             "Value": input[0], # gets the value from the user message
             "Type": type, # gets the type from the user message 
             "Date": None,
-            "Description": None
+            "Description": "Nenhuma"
         }
         return answer
     elif len(input) == 3: # will check if the user used three or four fields (VALOR TIPO DATA DESCRIÇÃO)
@@ -34,6 +46,7 @@ def checkInput (input): # check how the user made his input
         aux2 = aux[0].split("/", 2)
         if len(aux) == 1:
             if len(aux2) == 1:
+                print("O usuário digitou três campos, sendo DESCRIÇÃO o último e com uma palavra.")
                 answer = { 
                     "Value": input[0], # gets the value from the user message
                     "Type": type, # gets the type from the user message 
@@ -46,11 +59,12 @@ def checkInput (input): # check how the user made his input
                     datetime.strptime(formatedDate, "%Y-%m-%d")
                 except ValueError:
                     return "A data foi inserida no formato errado."
+                print("O usuário digitou três campos, sendo DATA o último")
                 answer = { 
                    "Value": input[0], # gets the value from the user message
                    "Type": type, # gets the type from the user message 
                    "Date": formatedDate, # gets the date from the user message
-                   "Description": None
+                   "Description": "Nenhuma"
                 }
             else:
                 return "A data foi inserida no formato errado."          
@@ -62,6 +76,7 @@ def checkInput (input): # check how the user made his input
                     datetime.strptime(formatedDate, "%Y-%m-%d")
                 except ValueError:
                     return "A data foi inserida no formato errado."
+                print("O usuário digitou os quatro campos.")
                 answer = { 
                     "Value": input[0], # gets the value from the user message
                     "Type": type, # gets the type from the user message 
@@ -69,6 +84,7 @@ def checkInput (input): # check how the user made his input
                     "Description": aux[1].capitalize() # gets the description from the user message
                 } 
             else: 
+                print("O usuário digitou três campos, sendo descrição o último e com mais de uma palavra.")
                 answer = { 
                     "Value": input[0], # gets the value from the user message
                     "Type": type, # gets the type from the user message 
@@ -76,4 +92,3 @@ def checkInput (input): # check how the user made his input
                     "Description": input[2].capitalize() # gets the description from the user message
                 }
             return answer
-             
