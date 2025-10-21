b3c403cbe0cca4a0961515b0353cb56f8dde8a08
otavioaugustoafm <otavioaugustoafm@gmail.com>
2025-10-21 15:16:33 -0300

    TestBot_v06

---
+++ FinanceBot/TestBot_v06/main.py
@@ -0,0 +1,145 @@
+from telegram.ext import Application, ConversationHandler, MessageHandler, CommandHandler, filters, ContextTypes
+from datetime import datetime
+from telegram import Update
+import validations
+import processing
+import database
+
+TOKEN = "N/A"
+
+removeMonth = ""
+
+GO_TO_SHOWEXPENSES, GO_TO_REMOVEEXPENSES, GO_TO_GETMONTH, GO_TO_GETIDS, GO_TO_SHOWALL = range(5)
+
+async def showMenu(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    try:
+        print("Mostrando menu.\n---------------------------------")
+        await update.message.reply_text("---------- Bot de finanças ----------\nInsira um gasto no seguinte modelo:\n\n VALOR TIPO DATA DESCRIÇÃO\n29,99 Compras 26/09/2005 Camiseta\n\nOs tipos disponíveis são: Transporte, Compras, Extra e Outros.\n\nDigite /1 mostrar os gastos e a soma deles em um mês.\n\nDigite /2 para filtrar gastos.\n\nDigite /3 para somar gastos.\n\nDigite /4 para remover um gasto.\n\nDigite /5 para mostrar todos os gastos.\n-------------------------------------")
+        return True
+    except Exception as e:
+        print(f"Erro ao mostrar o menu.\nErro: {e}\n--------------------")
+        return None
+
+async def storeExpenses(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    try:
+        input = update.message.text
+        input = processing.inputProcessing(input)
+        if isinstance(input, str):
+            await update.message.reply_text(input)
+            return
+        output = database.store(input)
+        if output is None:
+            await update.message.reply_text("Erro ao armazenar gasto.")
+            return    
+        await update.message.reply_text(output)
+    except Exception as e:
+        print(e)
+        return None
+    
+async def showAux(update:Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Digite o mês que quer visualizar.")
+    return GO_TO_SHOWEXPENSES
+
+async def showExpenses(update:Update, context: ContextTypes.DEFAULT_TYPE):
+    print("Apresentando gastos...")
+    try:
+        input = update.message.text
+        month = validations.validateMonth(input)
+        if month is False:
+            await update.message.reply_text("Mês inválido, tente novamente.")
+            return
+        date, nextDate = validations.validateDBDates(month)
+        search = database.getExpenses(date, nextDate)
+        output = f"--- Resumo dos Gastos no Mês de {input.capitalize()} ---\n" + search 
+        await update.message.reply_text(output)
+        print(search)
+        return ConversationHandler.END
+    except Exception as e:
+        print(f"Erro ao mostrar gastos.\nErro: {e}")
+        return None
+    
+async def removeAux1(update:Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Digite o mês que quer remover algum gasto.")
+    return GO_TO_GETMONTH
+
+async def removeAux2(update:Update, context: ContextTypes.DEFAULT_TYPE):
+    global removeMonth 
+    removeMonth = update.message.text
+    Month = validations.validateMonth(removeMonth)
+    if Month is False:
+        await update.message.reply_text("Mês inválido.")
+        return ConversationHandler.END
+    results = database.showAll(Month, "2")
+    if not results:
+        await update.message.reply_text("Nenhum gasto encontrado.")
+        return ConversationHandler.END        
+    output = processing.outputProcessing(results)
+    await update.message.reply_text(output)
+    await update.message.reply_text("Digite os IDs dos gastos que quer remover.\n\nUtilize uma barra de espaço entre eles e se atente para não digitar errado.")
+    return GO_TO_GETIDS
+
+async def removeExpenses(update:Update, context: ContextTypes.DEFAULT_TYPE):
+    print("Removendo gastos...")
+    try:
+        removeIDs = update.message.text
+        removeIDs = removeIDs.split(" ")
+        status = database.removeExpenses(removeIDs)
+        if status is True:
+            await update.message.reply_text("Remoção finalizada.")
+        return ConversationHandler.END
+    except Exception as e:
+        print(f"Erro ao remover gastos.\nErro: {e}")
+        return None
+    
+async def showAllAux(update:Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Digite o mês que quer visualizar os detalhes dos gastos.")
+    return GO_TO_SHOWALL
+
+async def showAllExpenses(update:Update, context: ContextTypes.DEFAULT_TYPE):
+    print("Mostrando gastos...")
+    try:
+        global removeMonth 
+        removeMonth = update.message.text
+        Month = validations.validateMonth(removeMonth)
+        if Month is False:
+            await update.message.reply_text("Mês inválido.")
+            return ConversationHandler.END
+        results = database.showAll(Month, "2")
+        if not results:
+            await update.message.reply_text("Nenhum gasto encontrado.")
+            return ConversationHandler.END               
+        output = processing.outputProcessing(results)
+        await update.message.reply_text(output)
+        return ConversationHandler.END
+    except Exception as e:
+        print(f"Erro ao mostrar gastos.\nErro: {e}")
+        return None
+
+def main():
+    try:
+        database.createTable()
+        application = Application.builder().token(TOKEN).build()
+        conv_handler = ConversationHandler(
+            entry_points = [
+                CommandHandler("1", showAux),
+                CommandHandler("2", showAllAux),
+                CommandHandler("3", removeAux1),
+                ],
+            states = {
+                GO_TO_SHOWEXPENSES: [MessageHandler(filters.TEXT & ~filters.COMMAND, showExpenses)],
+                GO_TO_GETMONTH: [MessageHandler(filters.TEXT & ~filters.COMMAND, removeAux2)],
+                GO_TO_GETIDS: [MessageHandler(filters.TEXT & ~filters.COMMAND, removeExpenses)],
+                GO_TO_SHOWALL: [MessageHandler(filters.TEXT & ~filters.COMMAND, showAllExpenses)]
+                },
+            fallbacks = []
+        )
+        application.add_handler(conv_handler)
+        application.add_handler(CommandHandler("start", showMenu))
+        application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, storeExpenses))
+        application.run_polling()
+    except Exception as e:
+        print(f"Erro no main.\nErro: {e}")
+        return None
+    
+if __name__ == "__main__":
+    main()
--- FinanceBot/TestBot_v06/validations.py
+++ FinanceBot/TestBot_v06/validations.py
@@ -4,7 +4,7 @@ currentMonth = datetime.now().month
 
 def validateDate(date):
     try:
-        print(f"Validando data: {date}")
+        print(f"\nValidando data: {date}")
         try:
             date = datetime.strptime(date, "%d/%m/%Y").date()
             dbDate = datetime.strftime(date, "%Y-%m-%d")
@@ -26,10 +26,57 @@ def validateDate(date):
                     return dbDate
                 except:
                     print("Data inválida.\n--------------------")
-                    return None
+                    return False
     except Exception as e:
-        print(f"Erro ao validar data. Erro:\n {e}")
+        print(f"Erro ao validar data.\nErro: {e}")
+        return None
 
-if __name__ == "__main__":
-    x = validateDate("30/9")
-    print(x)
+def validateType(type):
+    try:
+        print(f"\nValidando tipo: {type}")
+        TYPES = ["COMPRAS", "TRANSPORTE", "EXTRA", "OUTROS"]
+        type = type.upper()
+        if type in TYPES:
+            type = type.capitalize()
+            print(f"Tipo válido.\n--------------------")
+            return type
+        else:
+            type = type.capitalize()
+            print(f"Tipo inválido.\n--------------------")
+            return False
+    except Exception as e:
+        print(f"Erro ao checar tipo.\nErro: {e}\n--------------------")
+        return None
+    
+def validateMonth(month):
+    try:
+        print(f"\nValidando mês: {month}")
+        MONTHS = ["JANEIRO", "FEVEREIRO", "MARÇO", "ABRIL", "MAIO", "JUNHO", "JULHO", "AGOSTO", "SETEMBRO", "OUTUBRO", "NOVEMBRO", "DEZEMBRO"]
+        if month.upper() in MONTHS:
+            month = MONTHS.index(month.upper())
+            print("Mês válido.\n--------------------")
+            return month + 1
+        else:
+            print("Mês inválido.\n--------------------")
+            return False
+    except Exception as e:
+        print(f"Erro ao checar o mês.\nErro: {e}\n--------------------")
+        return None
+
+def validateDBDates(month):
+    try:
+        currentYear = datetime.now().year
+        date = f"2/{month}/{currentYear}"
+        nextDate = '3/'
+        if month == 12:
+            nextDate += f"1/{currentYear + 1}"
+        else:
+            nextDate += f"{month + 1}/{currentYear}"
+        date = datetime.strptime(date, "%d/%m/%Y").date()
+        date = datetime.strftime(date, "%Y-%m-%d")
+        nextDate = datetime.strptime(nextDate, "%d/%m/%Y").date()
+        nextDate = datetime.strftime(nextDate, "%Y-%m-%d")
+        return date, nextDate
+    except Exception as e:
+        print(f"Erro ao transformar o mês nas datas do BD.\nErro: {e}\n--------------------")
+        return None
